987c815eb2aa53446b9ab57b5c3136f02dcce408
malles <matthijs@bixie.nl>
2015-09-15 21:22:16 +0200

    cleanup

--- src/PortfolioImageHelper.php
+++ src/PortfolioImageHelper.php
@@ -17,19 +17,17 @@ class PortfolioImageHelper extends Helper {
 	/**
 	 * @param App $app
 	 */
-	public function __construct(App $app)
-	{
+	public function __construct (App $app) {
 		$this->app = $app;
 	}
 
 	/**
 	 * Set shortcuts.
-	 * @param       $name
+	 * @param string $name
 	 * @param array $args
 	 * @return bool|mixed
 	 */
-	public function __invoke($name, $args = [])
-	{
+	public function __invoke ($name, $args = []) {
 
 		if (is_callable([$this, $name])) {
 			return call_user_func_array([$this, $name], (array)$args);
@@ -40,21 +38,17 @@ class PortfolioImageHelper extends Helper {
 	/**
 	 * {@inheritdoc}
 	 */
-	public function getName()
-	{
+	public function getName () {
 		return 'portfolioimage';
 	}
 
-
 	/**
-	 * @param       $resource
+	 * @param array $resource
 	 * @param array $options
 	 * @return mixed
 	 */
 	public function url ($resource, $options = []) {
-
 		$resource = $this->validResource($resource);
-
 		if (!empty($options['width']) || !empty($options['height'])) {
 
 			if ($resized = $this->resizeImage($resource['src'], $options)) {
@@ -65,17 +59,27 @@ class PortfolioImageHelper extends Helper {
 		return $resource['src'];
 	}
 
-
+	/**
+	 * @param array $resource
+	 * @return array
+	 */
 	protected function validResource ($resource) {
 		$resource = array_replace(['src' => ''], $resource);
 		if (!file_exists($resource['src'])) {
+
 			$resource['src'] = '/packages/bixie/portfolio/assets/noimage.jpg';
 			$resource['filename'] = 'noimage.jpg';
 			$resource['title'] = 'No image found';
+
 		}
 		return $resource;
 	}
 
+	/**
+	 * @param string $source
+	 * @param array $options
+	 * @return bool|mixed
+	 */
 	protected function resizeImage ($source, $options) {
 		try {
 
@@ -105,8 +109,12 @@ class PortfolioImageHelper extends Helper {
 		}
 	}
 
+	/**
+	 * @param string $source
+	 * @param array $options
+	 * @return string
+	 */
 	protected function getCachePath ($source, $options) {
-
 		$folder = App::get('path.cache') . '/portfolio';
 		App::file()->makeDir($folder);
 
@@ -115,8 +123,11 @@ class PortfolioImageHelper extends Helper {
 		return "$folder/$cachename";
 	}
 
+	/**
+	 * @param array $options
+	 */
 	public static function clearCache ($options = []) {
-		// clear temp folder
+
 		if (@$options['temp']) {
 			App::file()->delete(App::get('path.cache') . '/portfolio');
 		}
