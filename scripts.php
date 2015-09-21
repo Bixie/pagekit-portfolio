5a99abbcfb1bd61f65886ca06fcfe6a27e2e7c1f
malles <matthijs@bixie.nl>
2015-09-21 19:35:05 +0200

    fixed from 0.9.2

--- scripts.php
+++ scripts.php
@@ -1,5 +1,4 @@
 <?php
-use Pagekit\Site\Model\Node;
 
 return [
 
@@ -24,29 +23,6 @@ return [
 				$table->addColumn('data', 'json_array', ['notnull' => false]);
 				$table->setPrimaryKey(['id']);
 			});
-		}
-		//temp fix trigger install node
-		$nodes = [
-
-			'portfolio' => [
-				'name' => '@portfolio',
-				'label' => 'Portfolio',
-				'controller' => 'Pagekit\\Portfolio\\Controller\\SiteController',
-				'protected' => true,
-				'frontpage' => true
-			]
-
-		];
-		foreach ($nodes as $type => $route) {
-			if (isset($route['protected']) and $route['protected'] and !Node::where(['type = ?'], [$type])->first()) {
-				Node::create([
-					'title' => $route['label'],
-					'slug' => $app->filter($route['label'], 'slugify'),
-					'type' => $type,
-					'status' => 1,
-					'link' => $route['name']
-				])->save();
-			}
 		}
 
     },
